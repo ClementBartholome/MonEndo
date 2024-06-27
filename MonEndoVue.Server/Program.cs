0306ab979d1b8aaef0c8bf0a0628b26a37c2d666
Clément Bartholomé <clementoss@gmail.com>
2024-06-27 21:25:35 +0200

    socle 0.1

--- MonEndoVue.Server/Program.cs
+++ MonEndoVue.Server/Program.cs
@@ -54,7 +54,7 @@ namespace MonEndoVue.Server
             {
                 options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.Preserve;
             });
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
 
