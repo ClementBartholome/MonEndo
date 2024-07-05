c40dfde5fdb6c390dade88bf5c847a01c0a8ed7f
Clément Bartholomé <clementoss@gmail.com>
2024-07-05 10:59:12 +0200

    fix(api): correction api url sur l'env de dev + ajout jwt key

--- monendovue.client/src/services/authService.ts
+++ monendovue.client/src/services/authService.ts
@@ -1,6 +1,6 @@
 ﻿import axios from 'axios';
 
-const API_URL = 'https://monendoapi.azurewebsites.net/';
+const API_URL = 'https://localhost:7206/';
 
 const authService = {
     async login(email: string, password: string) {
