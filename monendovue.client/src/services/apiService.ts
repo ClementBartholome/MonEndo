c900c68e83a28dc266414d9733694d551b3aa5e3
Clément Bartholomé <clementoss@gmail.com>
2024-07-07 13:37:18 +0200

    fix(api): correction api url sur l'env de prod

--- monendovue.client/src/services/apiService.ts
+++ monendovue.client/src/services/apiService.ts
@@ -1,6 +1,6 @@
 ﻿import axios from 'axios';
 
-const API_URL = 'https://localhost:7206/';
+const API_URL = 'https://monendoapi.azurewebsites.net/';
 
 const apiService = {
     async getDonneesCarnetSante(carnetSanteId: number) {
